6240d7cdb0f3f63f4dfadb278bd8d46511788501
Antonio Carlos <a.carlossf@hotmail.com>
2019-11-19 19:12:27 -0300

    Fim do primeiro capitulo

--- BitBank/Program.cs
+++ BitBank/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BitBank.Funcionarios;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,23 @@ namespace BitBank
     {
         static void Main(string[] args)
         {
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+
+            Funcionario carlos = new Funcionario();
+            carlos.Nome = "Antonio Carlos";
+            carlos.CPF = "032.437.855.61";
+            carlos.Salario = 3300;
+
+            Diretor barbara = new Diretor();
+            barbara.Nome = "Barbara Sousa";
+            barbara.CPF = "132.847.588.63";
+            barbara.Salario = 3000;
+
+            gerenciador.Registrar(carlos);
+            gerenciador.Registrar(barbara);
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+            Console.ReadKey();
         }
     }
 }
